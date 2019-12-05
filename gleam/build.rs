15b6082c82293291ee27b22ed01b60043558f0ed
Jeff Muizelaar <jrmuizel@gmail.com>
2019-12-05 08:51:33 -0500

    Delete tinyrenderer

--- gleam/build.rs
+++ gleam/build.rs
@@ -1,19 +1,10 @@
 extern crate gl_generator;
 extern crate cc;
 
-use std::env;
-use std::fs::File;
-use std::path::Path;
-
 fn main() {
     cc::Build::new()
         .cpp(true)
         .file("src/gl.cc")
-        .file("src/main.cc")
-        .file("src/model.cc")
-        .file("src/our_gl.cc")
-        .file("src/tgaimage.cc")
-        .file("src/geometry.cc")
         .define("FOO", Some("bar"))
         .flag("-std=c++14")
         .include("src")
