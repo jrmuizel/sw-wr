1b505fe337eae9f8d97a50b4c38df0b413a06906
Lee Salzman <lsalzman@gmail.com>
2020-03-02 11:52:15 -0500

    separate out GL defines into gl_defs.h

--- swgl/src/gl.cc
+++ swgl/src/gl.cc
@@ -32,32 +32,10 @@
 #define IMPLICIT
 #endif
 
+#include "gl_defs.h"
 #include "glsl.h"
 
 using namespace glsl;
-
-#define GL_ARRAY_BUFFER                   0x8892
-#define GL_ELEMENT_ARRAY_BUFFER           0x8893
-
-typedef int8_t GLbyte;
-typedef uint8_t GLubyte;
-typedef int16_t GLshort;
-typedef uint16_t GLushort;
-typedef int32_t GLint;
-typedef uint32_t GLuint;
-typedef int64_t GLint64;
-typedef uint64_t GLuint64;
-
-typedef float GLfloat;
-typedef double GLdouble;
-
-typedef uint32_t GLenum;
-typedef int32_t GLboolean;
-typedef uint32_t GLbitfield;
-
-typedef int32_t GLsizei;
-typedef size_t GLsizeiptr;
-typedef intptr_t GLintptr;
 
 struct IntRect {
     int x;
@@ -79,33 +57,6 @@ struct VertexAttrib {
         char *buf = nullptr; // XXX: this can easily dangle
         size_t buf_size = 0; // this will let us bounds check
 };
-#define GL_RGBA32F                        0x8814
-#define GL_RGBA8                          0x8058
-#define GL_R8                             0x8229
-#define GL_RGBA32I                        0x8D82
-#define GL_BGRA8                          0x93A1
-
-#define GL_BYTE                           0x1400
-#define GL_UNSIGNED_BYTE                  0x1401
-#define GL_SHORT                          0x1402
-#define GL_UNSIGNED_SHORT                 0x1403
-#define GL_INT                            0x1404
-#define GL_UNSIGNED_INT                   0x1405
-#define GL_FLOAT                          0x1406
-
-#define GL_RED                            0x1903
-#define GL_GREEN                          0x1904
-#define GL_BLUE                           0x1905
-#define GL_ALPHA                          0x1906
-#define GL_RGB                            0x1907
-#define GL_RGBA                           0x1908
-#define GL_RGBA_INTEGER                   0x8D99
-#define GL_BGRA                           0x80E1
-
-#define GL_DEPTH_COMPONENT                0x1902
-#define GL_DEPTH_COMPONENT16              0x81A5
-#define GL_DEPTH_COMPONENT24              0x81A6
-#define GL_DEPTH_COMPONENT32              0x81A7
 
 int bytes_for_internal_format(GLenum internal_format) {
         switch (internal_format) {
@@ -147,11 +98,6 @@ glsl::TextureFormat gl_format_to_texture_format(int type) {
         }
 }
 
-#define GL_QUERY_RESULT                   0x8866
-#define GL_QUERY_RESULT_AVAILABLE         0x8867
-#define GL_TIME_ELAPSED                   0x88BF
-#define GL_SAMPLES_PASSED                 0x8914
-
 struct Query {
         uint64_t value = 0;
 };
@@ -182,10 +128,6 @@ struct Buffer {
         }
 };
 
-#define GL_READ_FRAMEBUFFER               0x8CA8
-#define GL_DRAW_FRAMEBUFFER               0x8CA9
-#define GL_FRAMEBUFFER                    0x8D40
-
 struct Framebuffer {
         GLuint color_attachment = 0;
         GLint layer = 0;
@@ -198,18 +140,6 @@ struct Renderbuffer {
         ~Renderbuffer();
 };
 
-#define GL_NEAREST                  0x2600
-#define GL_LINEAR                   0x2601
-#define GL_NEAREST_MIPMAP_NEAREST   0x2700
-#define GL_NEAREST_MIPMAP_LINEAR    0x2702
-#define GL_LINEAR_MIPMAP_NEAREST    0x2701
-#define GL_LINEAR_MIPMAP_LINEAR     0x2703
-#define GL_TEXTURE_WRAP_S           0x2802
-#define GL_TEXTURE_WRAP_T           0x2803
-#define GL_TEXTURE_MAG_FILTER       0x2800
-#define GL_TEXTURE_MIN_FILTER       0x2801
-#define GL_CLAMP_TO_EDGE            0x812F
-
 glsl::TextureFilter gl_filter_to_texture_filter(int type) {
         switch (type) {
                 case GL_NEAREST: return glsl::TextureFilter::NEAREST;
@@ -221,10 +151,6 @@ glsl::TextureFilter gl_filter_to_texture_filter(int type) {
                 default: assert(0);  return glsl::TextureFilter::NEAREST;
         }
 }
-
-#define GL_TEXTURE_2D               0x0DE1
-#define GL_TEXTURE_3D               0x806F
-#define GL_TEXTURE_2D_ARRAY         0x8C1A
 
 struct Texture {
     int levels = 0;
@@ -322,8 +248,6 @@ struct ProgramImpl {
 
 typedef ProgramImpl* (*ProgramLoader)();
 
-#define GL_VERTEX_SHADER                  0x8B31
-#define GL_FRAGMENT_SHADER                0x8B30
 struct Shader {
     GLenum type = 0;
     ProgramLoader loader = nullptr;
@@ -464,28 +388,6 @@ Program::~Program() {
     delete frag_impl;
 }
 
-#define GL_BLEND                    0x0BE2
-#define GL_ZERO                     0
-#define GL_ONE                      1
-#define GL_SRC_COLOR                0x0300
-#define GL_ONE_MINUS_SRC_COLOR      0x0301
-#define GL_SRC_ALPHA                0x0302
-#define GL_ONE_MINUS_SRC_ALPHA      0x0303
-#define GL_DST_ALPHA                0x0304
-#define GL_ONE_MINUS_DST_ALPHA      0x0305
-#define GL_DST_COLOR                0x0306
-#define GL_ONE_MINUS_DST_COLOR      0x0307
-#define GL_CONSTANT_COLOR           0x8001
-#define GL_ONE_MINUS_CONSTANT_COLOR 0x8002
-#define GL_CONSTANT_ALPHA           0x8003
-#define GL_ONE_MINUS_CONSTANT_ALPHA 0x8004
-#define GL_SRC1_ALPHA               0x8589
-#define GL_SRC1_COLOR               0x88F9
-#define GL_ONE_MINUS_SRC1_COLOR     0x88FA
-#define GL_ONE_MINUS_SRC1_ALPHA     0x88FB
-
-#define GL_FUNC_ADD             0x8006
-
 // for GL defines to fully expand
 #define CONCAT_KEY(prefix, x, y, z, w, ...) prefix ## x ## y ## z ## w
 #define BLEND_KEY(...) CONCAT_KEY(BLEND_, __VA_ARGS__, 0, 0)
@@ -510,42 +412,7 @@ enum BlendKey : uint8_t
     FOR_EACH_BLEND_KEY(DEFINE_BLEND_KEY)
 };
 
-#define GL_NEVER                0x0200
-#define GL_LESS                 0x0201
-#define GL_EQUAL                0x0202
-#define GL_LEQUAL               0x0203
-#define GL_GREATER              0x0204
-#define GL_NOTEQUAL             0x0205
-#define GL_GEQUAL               0x0206
-#define GL_ALWAYS               0x0207
-#define GL_DEPTH_TEST           0x0B71
-
-#define GL_SCISSOR_TEST             0x0C11
-
-#define GL_TEXTURE0                       0x84C0
-#define GL_TEXTURE1                       0x84C1
-#define GL_TEXTURE2                       0x84C2
-#define GL_TEXTURE3                       0x84C3
-#define GL_TEXTURE4                       0x84C4
-#define GL_TEXTURE5                       0x84C5
-#define GL_TEXTURE6                       0x84C6
-#define GL_TEXTURE7                       0x84C7
-#define GL_TEXTURE8                       0x84C8
-#define GL_TEXTURE9                       0x84C9
-#define GL_TEXTURE10                      0x84CA
-#define GL_TEXTURE11                      0x84CB
-#define GL_TEXTURE12                      0x84CC
-#define GL_TEXTURE13                      0x84CD
-#define GL_TEXTURE14                      0x84CE
-#define GL_TEXTURE15                      0x84CF
-#define GL_MAX_TEXTURE_UNITS              0x84E2
-#define GL_MAX_TEXTURE_IMAGE_UNITS        0x8872
-
 const size_t MAX_TEXTURE_UNITS = 16;
-
-#define GL_RENDERBUFFER                   0x8D41
-#define GL_PIXEL_PACK_BUFFER              0x88EB
-#define GL_PIXEL_UNPACK_BUFFER            0x88EC
 
 template<typename T>
 static inline bool unlink(T& binding, T n) {
@@ -919,24 +786,9 @@ void Disable(GLenum cap) {
     }
 }
 
-#define GL_NO_ERROR                 0
-
 GLenum GetError() {
     return GL_NO_ERROR;
 }
-
-#define GL_VENDOR                   0x1F00
-#define GL_RENDERER                 0x1F01
-#define GL_VERSION                  0x1F02
-#define GL_EXTENSIONS               0x1F03
-#define GL_NUM_EXTENSIONS           0x821D
-#define GL_MAX_TEXTURE_SIZE         0x0D33
-#define GL_MAX_ARRAY_TEXTURE_LAYERS 0x88FF
-#define GL_DRAW_FRAMEBUFFER_BINDING 0x8CA6
-#define GL_READ_FRAMEBUFFER_BINDING 0x8CAA
-#define GL_DEPTH_WRITEMASK          0x0B72
-#define GL_PIXEL_PACK_BUFFER_BINDING      0x88ED
-#define GL_PIXEL_UNPACK_BUFFER_BINDING    0x88EF
 
 static const char * const extensions[] = {
     "GL_ARB_blend_func_extended",
@@ -1317,9 +1169,6 @@ void BindFramebuffer(GLenum target, GLuint fb) {
 void BindRenderbuffer(GLenum target, GLuint rb) {
     ctx->get_binding(target) = rb;
 }
-
-#define GL_UNPACK_ROW_LENGTH              0x0CF2
-#define GL_UNPACK_ALIGNMENT               0x0CF5
 
 void PixelStorei(GLenum name, GLint param) {
     if (name == GL_UNPACK_ALIGNMENT) {
@@ -1801,11 +1650,6 @@ void UniformMatrix4fv(GLint location,
         fragment_shader->set_uniform_matrix4fv(location, value);
 }
 
-
-
-#define GL_COLOR_ATTACHMENT0              0x8CE0
-#define GL_DEPTH_ATTACHMENT               0x8D00
-#define GL_STENCIL_ATTACHMENT             0x8D20
 void FramebufferTexture2D(
         GLenum target,
         GLenum attachment,
@@ -2040,13 +1884,6 @@ void SetTextureBuffer(GLuint texid, GLenum internal_format, GLsizei width, GLsiz
     set_tex_storage(t, 1, internal_format, width, height, !buf, buf, min_width, min_height);
 }
 
-#define GL_FRAMEBUFFER_COMPLETE                      0x8CD5
-#define GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT         0x8CD6
-#define GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT 0x8CD7
-#define GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER        0x8CDB
-#define GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER        0x8CDC
-#define GL_FRAMEBUFFER_UNSUPPORTED                   0x8CDD
-
 GLenum CheckFramebufferStatus(GLenum target) {
     Framebuffer *fb = get_framebuffer(target);
     if (!fb || !fb->color_attachment) {
@@ -2054,10 +1891,6 @@ GLenum CheckFramebufferStatus(GLenum target) {
     }
     return GL_FRAMEBUFFER_COMPLETE;
 }
-
-#define GL_COLOR_BUFFER_BIT         0x00004000
-#define GL_DEPTH_BUFFER_BIT         0x00000100
-#define GL_STENCIL_BUFFER_BIT       0x00000400
 
 static inline bool clear_requires_scissor(Texture& t) {
     return ctx->scissortest && (ctx->scissor.x > 0 || ctx->scissor.y > 0 || ctx->scissor.width < t.width || ctx->scissor.height < t.height);
@@ -2259,15 +2092,6 @@ void BlitFramebuffer(
         dstfb->color_attachment, GL_TEXTURE_2D_ARRAY, 0, dstX0, dstY0, dstfb->layer,
         dstWidth, dstHeight, 1);
 }
-
-#define GL_POINTS                         0x0000
-#define GL_LINES                          0x0001
-#define GL_LINE_LOOP                      0x0002
-#define GL_LINE_STRIP                     0x0003
-#define GL_TRIANGLES                      0x0004
-#define GL_TRIANGLE_STRIP                 0x0005
-#define GL_TRIANGLE_FAN                   0x0006
-#define GL_QUADS                          0x0007
 
 } // extern "C"
 
